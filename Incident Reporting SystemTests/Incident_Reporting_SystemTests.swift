fd51975865567f68422efe522b0b5f82e0c0d8b7
Wyman <wymanleejun@gmail.com>
2016-04-11 13:06:58 +0800

    Initial Commit

---
+++ Incident Reporting SystemTests/Incident_Reporting_SystemTests.swift
@@ -0,0 +1,36 @@
+//
+//  Incident_Reporting_SystemTests.swift
+//  Incident Reporting SystemTests
+//
+//  Created by Admin on 11/4/16.
+//  Copyright © 2016 Dreamsmart. All rights reserved.
+//
+
+import XCTest
+@testable import Incident_Reporting_System
+
+class Incident_Reporting_SystemTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
