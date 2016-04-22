2b6d4581c26a8a4efc56d60d97e5a0d6feaa267e
Wyman <wymanleejun@gmail.com>
2016-04-22 11:30:57 +0800

    Refined login

---
+++ Incident Reporting System/createDesignableUI.swift
@@ -0,0 +1,77 @@
+//
+//  UIView+Designable.swift
+//  CreatingDesignableUserInterfaces
+//
+//  Created by Scott Gardner on 2/2/16.
+//  Copyright © 2016 Scott Gardner. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class DesignableImageView: UIImageView { }
+@IBDesignable class DesignableButton: UIButton { }
+@IBDesignable class DesignableTextField: UITextField {
+    
+    @IBInspectable
+    var placeholderTextColor: UIColor = UIColor.lightGrayColor() {
+        didSet {
+            guard let placeholder = placeholder else {
+                return
+            }
+            
+            attributedPlaceholder = NSAttributedString(string: placeholder, attributes: [NSForegroundColorAttributeName: placeholderTextColor])
+        }
+    }
+    
+}
+
+extension UIView {
+    
+    @IBInspectable
+    var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        
+        set(newBorderWidth) {
+            layer.borderWidth = newBorderWidth
+        }
+    }
+    
+    @IBInspectable
+    var borderColor: UIColor? {
+        get {
+            return layer.borderColor != nil ? UIColor(CGColor: layer.borderColor!) : nil
+        }
+        
+        set {
+            layer.borderColor = newValue?.CGColor
+        }
+    }
+    
+    @IBInspectable
+    var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue != 0
+        }
+    }
+    
+    @IBInspectable
+    var makeCircular: Bool? {
+        get {
+            return nil
+        }
+        
+        set {
+            if let makeCircular = newValue where makeCircular {
+                cornerRadius = min(bounds.width, bounds.height) / 2.0
+            }
+        }
+    }
+    
+}
---
+++ Podfile
@@ -0,0 +1,18 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '9.0'
+use_frameworks!
+
+target 'Incident Reporting System' do
+
+pod 'Alamofire', '~> 3.3'
+
+end
+
+target 'Incident Reporting SystemTests' do
+
+end
+
+target 'Incident Reporting SystemUITests' do
+
+end
+
