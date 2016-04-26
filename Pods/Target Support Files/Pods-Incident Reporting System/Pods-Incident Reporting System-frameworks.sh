79f96aeee6c51654795e38844b06055e14dffd9f
Wyman <wymanleejun@gmail.com>
2016-04-26 16:46:59 +0800

    Added swiftyjson

--- Pods/Target Support Files/Pods-Incident Reporting System/Pods-Incident Reporting System-frameworks.sh
+++ Pods/Target Support Files/Pods-Incident Reporting System/Pods-Incident Reporting System-frameworks.sh
@@ -85,7 +85,9 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "Pods-Incident Reporting System/Alamofire.framework"
+  install_framework "Pods-Incident Reporting System/SwiftyJSON.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "Pods-Incident Reporting System/Alamofire.framework"
+  install_framework "Pods-Incident Reporting System/SwiftyJSON.framework"
 fi
